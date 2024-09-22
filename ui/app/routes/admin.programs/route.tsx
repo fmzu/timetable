71fb2b104f46255070a5a983f448784a056b7e50
fmzu <149039422+fmzu@users.noreply.github.com>
2024-09-23 02:09:37 +0900

    update: 講義の追加ページを作成する

--- ui/app/routes/admin.programs/route.tsx
+++ ui/app/routes/admin.programs/route.tsx
@@ -1,7 +1,137 @@
+import { useNavigate } from "@remix-run/react"
+import { useMutation } from "@tanstack/react-query"
+import { useState } from "react"
+import { Button } from "~/components/ui/button"
+import { Input } from "~/components/ui/input"
+import { Textarea } from "~/components/ui/textarea"
+import { client } from "~/lib/client"
+
 /**
  * 管理者が授業やユーザーを管理するページ
  * @returns
  */
 export default function Route() {
-  return <div>{"adminPage"}</div>
+  const navigate = useNavigate()
+
+  const [name, setName] = useState("")
+
+  const [timeSlot, setTimeSlot] = useState(0)
+
+  const [weekSlot, setWeekSlot] = useState(0)
+
+  const [ownerId, setOwnerId] = useState("")
+
+  const [unitsCount, setUnitsCount] = useState(0)
+
+  const [overview, setOverview] = useState("")
+
+  const [year, setYear] = useState(0)
+
+  const [period, setPeriod] = useState(0)
+
+  const mutation = useMutation({
+    async mutationFn() {
+      const resp = await client.api.admin.programs.$post({
+        json: {
+          name: name,
+          timeSlot: timeSlot,
+          weekSlot: weekSlot,
+          ownerId: ownerId,
+          unitsCount: unitsCount,
+          overview: overview,
+          year: year,
+          period: period,
+        },
+      })
+      const json = await resp.json()
+      return json
+    },
+  })
+
+  const onSubmit = () => {
+    const result = mutation.mutate()
+    if (result === null) {
+      navigate("/admin")
+      return
+    }
+  }
+
+  return (
+    <div className="p-4">
+      <form
+        className="space-y-2"
+        onSubmit={(event) => {
+          event.preventDefault()
+          onSubmit()
+        }}
+      >
+        <Input
+          type={"text"}
+          placeholder="名前"
+          value={name}
+          onChange={(event) => {
+            setName(event.target.value)
+          }}
+        />
+        <Input
+          type={"number"}
+          placeholder="時間枠（時間）"
+          value={timeSlot}
+          onChange={(event) => {
+            setTimeSlot(event.target.valueAsNumber)
+          }}
+        />
+        <Input
+          type={"number"}
+          placeholder="時間枠（曜日）"
+          value={weekSlot}
+          onChange={(event) => {
+            setWeekSlot(event.target.valueAsNumber)
+          }}
+        />
+        <Input
+          type={"text"}
+          placeholder="担当教員"
+          value={ownerId}
+          onChange={(event) => {
+            setOwnerId(event.target.value)
+          }}
+        />
+        <Input
+          type={"number"}
+          placeholder="単位数"
+          value={unitsCount}
+          onChange={(event) => {
+            setUnitsCount(event.target.valueAsNumber)
+          }}
+        />
+        <Textarea
+          placeholder="授業概要"
+          value={overview}
+          onChange={(event) => {
+            setOverview(event.target.value)
+          }}
+        />
+        <Input
+          type={"number"}
+          placeholder="実施時期（年度）"
+          value={year}
+          onChange={(event) => {
+            setYear(event.target.valueAsNumber)
+          }}
+        />
+        <Input
+          type={"number"}
+          placeholder="実施時期（前期後期）"
+          value={period}
+          onChange={(event) => {
+            setPeriod(event.target.valueAsNumber)
+          }}
+        />
+        <Button type={"submit"} className="w-full">
+          {"登録する"}
+        </Button>
+      </form>
+    </div>
+  )
 }
--- ui/app/routes/admin/route.tsx
+++ ui/app/routes/admin/route.tsx
@@ -1,7 +1,9 @@
+import { Outlet } from "@remix-run/react"
+
 /**
  * 管理者が授業やユーザーを管理するページ
  * @returns
  */
 export default function Route() {
-  return <div>{"adminPage"}</div>
+  return <Outlet />
 }
